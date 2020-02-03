38fde100047f2fff050fcb2db0c864fba0d5b77a
German.Samolazov <German.Samolazov@GSAMOLAZOVPC>
2020-02-03 14:19:28 +0300

    fix Views

---
+++ Totalizator/Scripts/Services/CookieService.js
@@ -0,0 +1,5 @@
+﻿function getCookiePartByKey(key) {
+    var value = "; " + document.cookie;
+    var parts = value.split("; " + key + "=");
+    if (parts.length == 2) return parts.pop().split(";").shift();
+}
