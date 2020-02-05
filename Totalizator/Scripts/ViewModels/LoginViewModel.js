c2aeb1c84dcf713e683779bdc1dec4d99ab023e1
German.Samolazov <German.Samolazov@GSAMOLAZOVPC>
2020-02-05 13:24:32 +0300

    add LogOut

--- Totalizator/Scripts/ViewModels/LoginViewModel.js
+++ Totalizator/Scripts/ViewModels/LoginViewModel.js
@@ -12,11 +12,9 @@
         self.LoginClick = function () {
             loginService.signIn(self.UserName(), self.UserPassword());
             var user = getCurrentUser();
-            console.log(user);
-            alert(user.UserName);
             $("#currentUserNameLayout").html(user.UserName);
             $("#profileElement").css("visibility", "visible");
-         //   location.href = "/Home/Main";
+           location.href = "/Home/Main";
         }
 
         self.RegisrtationClick = function () {
@@ -25,3 +23,9 @@
 
     }
 }
+
+//TODO сделать вывод коэффициентов 
+//TODO написать for в label для id 
+//TODO сделать admin Panel с раздчачей админок и тд
+//TODO нарисовать нормально кнопку профиля 
+//TODO нарисовать элемент ставки 
